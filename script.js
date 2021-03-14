60cb8c41260be80eb60bc07fb025f231efd24761
Frostwrath <nbainbridge@outlook.com.au>
2021-03-14 22:19:21 +1100

    Changes to script.js (renamed variables), README.md updated to include project overview and functionalitly text

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ var passwordContents;
 var wantLength;
 var pwArray = []
 
+//Start the password query and generation
 function generatePassword() {
-  var allchars = confirm("Do you want your password to have numbers, letters and symbols? (OK is Yes, Cancel is No, and will lead you to next prompt.)")
-  if (allchars == true) {
+  //Generate a password with all types of characters
+  var passChars = confirm("Do you want your password to have numbers, letters and symbols? (OK is Yes, Cancel is No, and will lead you to next prompt.)")
+  if (passChars == true) {
     passwordContents = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","!","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]
     wantLength = Number(prompt("How long do you want your password to be?"))
     for (let index = 0; index < wantLength; index++) {
@@ -19,8 +21,9 @@ function generatePassword() {
     return (password)
   }
   else {
-    var noSymbols = confirm("Do you want your password to be have numbers and letters, but no symbols?")
-    if (noSymbols==true) {
+    //Generate a password with numbers and all cases of letters
+    var passChars = confirm("Do you want your password to be have numbers and letters, but no symbols?")
+    if (PassChars==true) {
       passwordContents = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
       wantLength = Number(prompt("How long do you want your password to be?"))
       for (let index = 0; index < wantLength; index++) {
@@ -32,8 +35,9 @@ function generatePassword() {
       return (password)
     } 
     else {
-      var noSymbols = confirm("Do you want your password to be have numbers and lowercase, but no symbols or uppercase?")
-      if (noSymbols==true) {
+      //Generate a password with numbers and lowercase
+      var passChars = confirm("Do you want your password to be have numbers and lowercase, but no symbols or uppercase?")
+      if (passChars==true) {
         passwordContents = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
         wantLength = Number(prompt("How long do you want your password to be?"))
         for (let index = 0; index < wantLength; index++) {
@@ -45,8 +49,9 @@ function generatePassword() {
         return (password)
       } 
       else {
-        var noSymbols = confirm("Do you want your password to be have numbers and uppercase, but no symbols or lowercase?")
-        if (noSymbols==true) {
+        //Generate a password with numbers and uppercase
+        var passChars = confirm("Do you want your password to be have numbers and uppercase, but no symbols or lowercase?")
+        if (passChars==true) {
           passwordContents = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
           wantLength = Number(prompt("How long do you want your password to be?"))
           for (let index = 0; index < wantLength; index++) {
@@ -58,8 +63,9 @@ function generatePassword() {
           return (password)
         } 
         else {
-          var noSymbols = confirm("Do you want your password to be have all letters, but no symbols or numbers?")
-          if (noSymbols==true) {
+          //Generate a password with all cases of letters
+          var passChars = confirm("Do you want your password to be have all letters, but no symbols or numbers?")
+          if (passChars==true) {
             passwordContents = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
             wantLength = Number(prompt("How long do you want your password to be?"))
             for (let index = 0; index < wantLength; index++) {
@@ -71,8 +77,9 @@ function generatePassword() {
             return (password)
           } 
           else {
-            var noSymbols = confirm("Do you want your password to be have only lowercase?")
-            if (noSymbols==true) {
+            //Generate a password with lowercase only
+            var PassChars = confirm("Do you want your password to be have only lowercase?")
+            if (PassChars==true) {
               passwordContents = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
               wantLength = Number(prompt("How long do you want your password to be?"))
               for (let index = 0; index < wantLength; index++) {
@@ -84,8 +91,9 @@ function generatePassword() {
               return (password)
             } 
             else {
-              var noSymbols = confirm("Do you want your password to be have only uppercase?")
-              if (noSymbols==true) {
+              //Generate a password with uppercase only
+              var PassChars = confirm("Do you want your password to be have only uppercase?")
+              if (PassChars==true) {
                 passwordContents = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
                 wantLength = Number(prompt("How long do you want your password to be?"))
                 for (let index = 0; index < wantLength; index++) {
@@ -97,8 +105,9 @@ function generatePassword() {
                 return (password)
               } 
               else{
-                var allchars = confirm("Do you want your password to have only symbols?")
-                if (allchars == true) {
+                //Generate a password with symbols only
+                var PassChars = confirm("Do you want your password to have only symbols?")
+                if (passChars == true) {
                   passwordContents = ["!","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]
                   wantLength = Number(prompt("How long do you want your password to be?"))
                   for (let index = 0; index < wantLength; index++) {
@@ -110,6 +119,7 @@ function generatePassword() {
                 return (password)
                 } 
                 else {
+                  //Generate a password with numbers only
                   alert("Your password will consist of only numbers.")
                   wantLength = Number(prompt("How long do you want your password to be?"))
                   for (let index = 0; index < wantLength; index++) {
@@ -128,6 +138,8 @@ function generatePassword() {
     }
   }
 }
+
+
 // Write password to the #password input
 function writePassword() {
   password = generatePassword();
